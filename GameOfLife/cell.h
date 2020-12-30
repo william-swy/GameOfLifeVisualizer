f20a77fcb02c0032e06397e852a03769b30d8014
BigBlue <shenpaiwilliam@gmail.com>
2020-12-29 17:01:41 -0800

    Doc: Add Documentation For GUI Classes and Functions

--- GameOfLife/cell.h
+++ GameOfLife/cell.h
@@ -13,18 +13,53 @@ class Cell : public QGraphicsObject {
     Q_OBJECT
 public:
     Cell(int x, int y, int size, QGraphicsItem* parent = nullptr);
+
+    /**
+     * Returns the bounding box of the current cell.
+     *
+     * @return A QRectF that represents the current cell's bounding box
+     */
     QRectF boundingRect() const override;
+
+    /**
+     * Paints the current cell with a black boarder. If the cell state is dead ('alive == false`), the inside of the
+     * cell is painted white. If the cell state is alive (`alive == true`), the inside of the cell is painted black.
+     * @param painter
+     * @param option
+     * @param widget
+     */
     void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override;
 
 public slots:
+    /**
+     * Resets the current cell state to dead. Redraws changes if necessary.
+     */
     void resetCell();
+
+    /**
+     * Updates the current cell state by checking the board cell at coordinates (xCoord, yCoord).
+     * Redraws the changes if necessary.
+     *
+     * @param board The gameBoard to check if the current cell has changed states.
+     */
     void updateCell(gameBoard::GameBoard* board);
 
 signals:
+    /**
+     * A signal notifying that this cell has changed state.
+     *
+     * @param x The x coordinate of this cell.
+     * @param y The y coordinate of this cell.
+     */
     void cellChanged(int x, int y);
 
 protected:
-    //void advance(int phase) override;
+    /**
+     * Handles the event where this cell object is double clicked by mouse. Flips the current state of this cell and
+     * re-draws.
+     *
+     * @param event The double mouse click event.
+     */
     void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event) override;
 
 private:
--- GameOfLife/view.h
+++ GameOfLife/view.h
@@ -14,9 +14,17 @@ public:
     explicit View(QWidget* parent = nullptr);
 
 public slots:
+    /**
+     * Resets app zoom level to default.
+     */
     void resetZoom();
 
 protected:
+    /**
+     * Event listener to change app zoom level based on wheel scroll event. If consecutive zoom in or zoom out times
+     * exceeds, `maxZoomInTimes` stop zoom in or zoom out respectively.
+     * @param event The mouse wheel scroll event.
+     */
     void wheelEvent(QWheelEvent* event) override;
 
 private:
