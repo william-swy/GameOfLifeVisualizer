48f6779b46ca776e7a9c04166d9c9ca629fd7aae
Bigblue <shenpaiwilliam@gmail.com>
2021-07-29 18:46:59 -0600

    Rework: Automaton Simulation Speed Adjustment

--- src/visualization/dropdown_menu.cpp
+++ src/visualization/dropdown_menu.cpp
@@ -4,6 +4,7 @@
 #include <QIcon>
 #include <QMenu>
 #include <QString>
+#include <QTimer>
 #include <QWidget>
 
 FileMenu::FileMenu(const QString& title, QWidget* parent)
@@ -42,7 +43,9 @@ RunMenu::RunMenu(const QString& title, QWidget* parent)
       increase_speed(new QAction(QAction::tr("Increase Speed"))),
       decrease_speed(new QAction(QAction::tr("Decrease Speed"))),
       reset_speed(new QAction(QAction::tr("Reset Speed"))),
-      reset_board(new QAction(QAction::tr("Reset Board"))) {
+      reset_board(new QAction(QAction::tr("Reset Board"))),
+      timer(new QTimer(this)),
+      curr_delay_time(default_delay_time) {
   addAction(step);
   addAction(run);
   addAction(stop);
@@ -50,8 +53,71 @@ RunMenu::RunMenu(const QString& title, QWidget* parent)
   addAction(decrease_speed);
   addAction(reset_speed);
   addAction(reset_board);
+  connect(run, SIGNAL(triggered()), this, SLOT(run_automaton()));
+  connect(stop, SIGNAL(triggered()), this, SLOT(stop_automaton()));
+  stop->setEnabled(false);
+  connect(increase_speed, SIGNAL(triggered()), this, SLOT(increase_run_speed()));
+  connect(decrease_speed, SIGNAL(triggered()), this, SLOT(decrease_run_speed()));
+  connect(reset_speed, SIGNAL(triggered()), this, SLOT(reset_run_speed()));
 }
 
 void RunMenu::link_board(const BoardModel* model) noexcept {
   connect(step, SIGNAL(triggered()), model, SLOT(next_state()));
+  connect(reset_board, SIGNAL(triggered()), model, SLOT(reset_board()));
+  connect(timer, SIGNAL(timeout()), model, SLOT(next_state()));
+}
+
+void RunMenu::run_automaton() {
+  step->setEnabled(false);
+  run->setEnabled(false);
+  stop->setEnabled(true);
+  increase_speed->setEnabled(false);
+  decrease_speed->setEnabled(false);
+  reset_speed->setEnabled(false);
+  reset_board->setEnabled(false);
+
+  timer->start(curr_delay_time);
+}
+
+void RunMenu::change_speed_option() noexcept {
+  if (curr_delay_time == min_delay_time) {
+    increase_speed->setEnabled(false);
+    decrease_speed->setEnabled(true);
+  } else if (curr_delay_time == max_delay_time) {
+    increase_speed->setEnabled(true);
+    decrease_speed->setEnabled(false);
+  } else {
+    increase_speed->setEnabled(true);
+    decrease_speed->setEnabled(true);
+  }
+}
+
+void RunMenu::stop_automaton() {
+  timer->stop();
+  step->setEnabled(true);
+  run->setEnabled(true);
+  stop->setEnabled(false);
+  increase_speed->setEnabled(true);
+  decrease_speed->setEnabled(true);
+  reset_speed->setEnabled(true);
+  reset_board->setEnabled(true);
+}
+
+void RunMenu::increase_run_speed() {
+  if (curr_delay_time > min_delay_time) {
+    curr_delay_time /= time_scaling_factor;
+    change_speed_option();
+  }
+}
+
+void RunMenu::decrease_run_speed() {
+  if (curr_delay_time < max_delay_time) {
+    curr_delay_time *= time_scaling_factor;
+    change_speed_option();
+  }
+}
+
+void RunMenu::reset_run_speed() {
+  curr_delay_time = default_delay_time;
+  change_speed_option();
 }
--- src/visualization/dropdown_menu.h
+++ src/visualization/dropdown_menu.h
@@ -3,6 +3,7 @@
 #include <QAction>
 #include <QMenu>
 #include <QString>
+#include <QTimer>
 #include <QWidget>
 
 #include "board_model.h"
@@ -51,4 +52,25 @@ public:
   QAction* decrease_speed;
   QAction* reset_speed;
   QAction* reset_board;
+
+private:
+  QTimer* timer;
+  quint64 default_delay_time{1000};
+  quint64 curr_delay_time;
+  quint64 min_delay_time{125};
+  quint64 max_delay_time{8000};
+  quint64 time_scaling_factor{2};
+
+  void change_speed_option() noexcept;
+
+private slots:
+  void run_automaton();
+
+  void stop_automaton();
+
+  void increase_run_speed();
+
+  void decrease_run_speed();
+
+  void reset_run_speed();
 };
