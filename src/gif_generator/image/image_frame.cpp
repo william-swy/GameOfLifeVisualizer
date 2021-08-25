9d6984edba07b66320ee44fe6e3ccbb494dc135d
Bigblue <shenpaiwilliam@gmail.com>
2021-08-24 19:06:42 -0600

    Remove Old KD-Tree File

--- src/gif_generator/image/image_frame.cpp
+++ src/gif_generator/image/image_frame.cpp
@@ -5,14 +5,13 @@
 #include <tuple>
 #include <vector>
 
-#include "colour_kd_tree.h"
-
 namespace gif {
   namespace image {
     ImageFrame::ImageFrame(const unsigned char* byte_data, std::size_t num_bytes,
                            unsigned short width, unsigned short height)
         : width(width), height(height) {
       constexpr std::size_t step_size = 3;
+      pixel_data.reserve(num_bytes / step_size);
       for (std::size_t byte = 0; byte < num_bytes; byte += step_size) {
         pixel_data.emplace_back(byte_data[byte + 0], byte_data[byte + 1], byte_data[byte + 2]);
       }
